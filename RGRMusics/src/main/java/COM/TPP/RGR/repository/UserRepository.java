f7ed58814a43371c8717c865fb68badfc11ae913
Дроган Роман <135826856+W1nggy@users.noreply.github.com>
2025-02-05 17:15:54 +0200

    RGRMusic

--- RGRMusics/src/main/java/COM/TPP/RGR/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package COM.TPP.RGR.repository;
-
-import COM.TPP.RGR.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
